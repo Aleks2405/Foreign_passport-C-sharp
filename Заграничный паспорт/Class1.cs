aca87a29197fb12c6871f57848fb694d8fe3d598
Goro_sb71@stud.msk.itstep.org <Goro_sb71@stud.msk.itstep.org>
2022-01-28 16:37:17 +0300

    Добавьте файлы проекта.

---
+++ Заграничный паспорт/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passport
+{
+    class Foreign_passport
+    {
+        private string FIO;
+        private int series;
+        private string date_of_issue;
+       public Foreign_passport(string FIO,int series,string date_of_issue) 
+        {
+            this.FIO = FIO;
+            this.series = series;
+            this.date_of_issue = date_of_issue;
+        }
+         public void Print()
+        {
+            Console.WriteLine($"ФИО: {FIO}, серия паспорта: {series},дата выдачи: {date_of_issue}");
+        }
+    }
+}
---
+++ Заграничный паспорт/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Passport;
+/*Создайте класс «Заграничный паспорт». Вам необходимо
+хранить информацию о номере паспорта, ФИО владельца,
+дате выдачи и т.д. Предусмотреть механизмы для инициализации полей класса. Если значение для инициализации
+неверное, генерируйте исключение.*/
+namespace Заграничный_паспорт
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FIO, date;
+            int serial = 0;
+            Console.WriteLine("Введите ФИО:");
+            FIO = Console.ReadLine();
+            Console.WriteLine("Введите серию:");
+            serial = Int32.Parse(Console.ReadLine()); 
+            Console.WriteLine("Введите дату выдачи:");
+            date = Console.ReadLine();
+
+            Foreign_passport name = new Foreign_passport(FIO,serial,date);
+            try
+            {
+                //serial = Int32.Parse(Console.ReadLine());
+                serial = 0;
+            }
+            catch 
+            {
+                Console.WriteLine("Вы не правильно ввели серию паспорта");
+            }
+           
+            name.Print();
+            
+
+        }
+    }
+}
